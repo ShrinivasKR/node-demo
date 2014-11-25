a36d0325a3537f119163fe26a1f6ed2345ee446e
Shrinivas Kopparam Ramanath <shrini@uw.edu>
2014-11-25 12:11:15 -0800

    End of class after creating a basic node server, before node controller. Error 404 not working

--- hello-server.js
+++ hello-server.js
@@ -6,3 +6,28 @@
 
 'use strict';
 
+//var http = require('http');
+//
+//var server = http.createServer(function(req, res) {
+//    res.writeHead(200, {'Content-Type': 'text/plain'});
+//    //res.end('Hello World from Shrinivas!\n);
+//    res.end('you requested ' + req.url);
+//});
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello World from Shrinivas!')
+});
+
+app.get('/foo', function(req, res) {
+    res.send('You requested /foo!');
+});
+
+//server.listen(8080, function() {
+//   console.log('Server listening on http://localhost:8080');
+//});
+
+app.listen(8080, function() {
+   console.log('Server listening on http://localhost:8080');
+});
